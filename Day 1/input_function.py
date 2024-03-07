95b48fc73baad17ca550b81da70e3e6b57175f58
rohan-moovaliya <rohanmovaliya65@gmail.com>
2024-03-07 23:20:11 +0530

    Add Day-1 work

---
+++ Day 1/input_function.py
@@ -0,0 +1,7 @@
+print("Hello " + input("Enter your name: ") + " !")
+
+print(input("Enter your name: "))
+# input("Enter your name: " ==> convert into ==> input
+
+
+print(len(input("Enter your name: ")))
